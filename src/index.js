47be24f112b2d19275e7812fbaf134caac5257fc
Luan <luan.sergio@novahaus.com.br>
2022-01-08 20:02:29 -0300

    feat: handle query params

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 const http = require("http");
+const { URL } = require("url");
+
 const port = 3000;
 
 const routes = require("./routes");
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint ${request.url}`);
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+
+  console.log(
+    `Request method: ${request.method} | Endpoint ${parsedUrl.pathname}`
+  );
 
   const route = routes.find(
-    (route) => route.endpoint === Request.url && route.method === request.method
+    (route) =>
+      route.endpoint === parsedUrl.pathname && route.method === request.method
   );
 
   if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+
     route.handler(request, response);
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
-    response.end(`Cannot ${request.method} ${request.url}`);
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
   }
 });
 
